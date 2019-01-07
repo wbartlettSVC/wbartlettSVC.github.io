21228937abc6b061299f5b9f2026fe2f7421cde8
wbartlettSVC <45183174+wbartlettSVC@users.noreply.github.com>
2019-01-07 15:27:17 -0800

    js v.1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function main() {
+	var timeHours = new Date().getHours();
+	var message;
+
+	if(timeHours < 11) {
+		message = "Good Morning.";
+	}
+	else if(timeHours >= 11 || timeHours <= 17) {
+		message = "Good Afternoon.";
+	}
+	else {
+		message = "Good Evening.";
+	}
+	document.getElementById("message").innerHTML = message;
+}
