c7cf5ab9d1993a5e84798c2a74258781aa2de749
wbartlettSVC <45183174+wbartlettSVC@users.noreply.github.com>
2019-01-22 13:06:39 -0800

    v.4

--- js/decisionLoop.js
+++ js/decisionLoop.js
@@ -2,26 +2,27 @@ var table = 7;
 var arr = ['addition','subtraction','multiplication','division']
 var operator = arr[Math.floor(Math.random() * 4)];
 var num = 1;
+var numF = Math.floor(Math.random() * 101);
 var msg = '';
 
 function calc() {
 	if(operator === arr[0]) {
-		while(num < 11) {
+		while(num < numF) {
 			msg = num + ' + ' + table + ' = ' + (num + table) + '<br/>';
 			num++;
 		}
 	}else if(operator === arr[1]) {
-		while(num < 11) {
+		while(num < numF) {
 			msg = num + ' - ' + table + ' = ' + (num - table) + '<br/>';
 			num++;
 		}
 	}else if(operator === arr[2]) {
-		while(num < 11) {
+		while(num < numF) {
 			msg = num + ' x ' + table + ' = ' + (num * table) + '<br/>';
 			num++;
 		}
 	}else {
-		while(num < 11) {
+		while(num < numF) {
 			msg = num + ' ÷ ' + table + ' = ' + (num / table) + '<br/>';
 			num++;
 		}
