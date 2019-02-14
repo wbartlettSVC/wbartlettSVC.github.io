201180b814f6136aa7fd2e9f5720130bbe1ea4be
wbartlettSVC <45183174+wbartlettSVC@users.noreply.github.com>
2019-02-14 15:28:42 -0800

    v9/10

---
+++ js/JQueryUIStuff.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	
+	$("#anim").draggable({cancel:"#click"});
+	$("header > a > h1").draggable();
+	$("header > a > img").draggable();
+	$("footer").draggable();
+	$("#message").draggable();
+});
--- js/jQuery.js
+++ js/jQuery.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$("#click").click(function() {
 		$("#animInside").slideToggle("slow");
 	});
+	
+	$("body").hide();
+	$("body").fadeIn(1000);
 });
